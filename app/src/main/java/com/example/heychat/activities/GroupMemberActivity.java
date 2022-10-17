3d150fbbb824e3fff2521ca6db26b9157494b1fe
caoduczpsm <nguyencaoduc0922@gmail.com>
2022-10-17 13:44:01 +0700

    update exit group and dibanding group

---
+++ app/src/main/java/com/example/heychat/activities/GroupMemberActivity.java
@@ -0,0 +1,14 @@
+package com.example.heychat.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GroupMemberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_member);
+    }
+}
---
+++ app/src/main/java/com/example/heychat/activities/InfoGroupActivity.java
@@ -0,0 +1,14 @@
+package com.example.heychat.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InFoGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_fo_group);
+    }
+}
